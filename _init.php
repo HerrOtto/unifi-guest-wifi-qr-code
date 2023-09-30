fff95a69c2835a5fec273c03327222e28c2466c2
Tim David Saxen <info@herrotto.de>
2023-09-30 15:04:02 +0200

    Added multi site support

--- _init.php
+++ _init.php
@@ -1,8 +1,21 @@
 <?php
 
 $config = array(
-    'ip' => 'IP_OF_UNIFI_CLOUD_KEY',
-    'user' => 'UNIFI_USERNAME',
-    'pass' => 'UNIFI_PASSWORD',
-    'wifi-prefix' => 'GUEST_WIFI_PREFIX'
+
+    'siteName1' => array(
+	    'ip' => 'IP_OF_UNIFI_CLOUD_KEY',
+	    'user' => 'UNIFI_USERNAME',
+        'pass' => 'UNIFI_PASSWORD',
+        'wifi-prefix' => 'GUEST_WIFI_PREFIX'
+    ),
+
+    'siteName2' => array(
+	    'ip' => 'IP_OF_UNIFI_CLOUD_KEY',
+	    'user' => 'UNIFI_USERNAME',
+        'pass' => 'UNIFI_PASSWORD',
+        'wifi-prefix' => 'GUEST_WIFI_PREFIX'
+    )
+
+    //, ... more sites ...
+
 );

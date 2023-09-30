7455b2a3fdb320f49286ad7f5a69bfc651e6878f
Tim David Saxen <info@herrotto.de>
2023-09-30 15:04:35 +0200

    Added multi site support

--- newPSK.php
+++ newPSK.php
@@ -5,51 +5,53 @@ require_once __DIR__ . '/_init.php';
 require_once __DIR__ . '/_global.php';
 require_once __DIR__ . '/_unifi.class.php';
 
-try {
+foreach ($config as $configSiteName => $configSite) {
+    try {
 
-    // Create UniFI Client instance
-    $unifi = new UniFI();
+        // Create UniFI Client instance
+        $unifi = new UniFI();
 
-    // Login to UniFI
-    $res = $unifi->login(
-        $config['ip'],
-        $config['user'],
-        $config['pass']
-    );
-    if ($res === false) {
-        throw new Exception('Auth failed');
-    }
-
-    // Fetch list of WLAN networks
-    $wifis = $unifi->get(
-        '/proxy/network/v2/api/site/default/wlan/enriched-configuration'
-    );
-    if (count($config) < 1) {
-        throw new Exception('No configuration found');
-    }
-
-    // Change WLAN keys
-    foreach ($wifis as $wifisI => $wifisData) {
-        if (strpos($wifisData['configuration']['name'], $config['wifi-prefix']) !== 0) {
-            continue;
+        // Login to UniFI
+        $res = $unifi->login(
+            $configSite['ip'],
+            $configSite['user'],
+            $configSite['pass']
+        );
+        if ($res === false) {
+            throw new Exception('Auth failed');
         }
 
-        // Generate new SSID and PSK
-        $newSSID = $config['wifi-prefix'] . ' ' . date('ymd');
-        $newPsk = generateRandomWord(5, 6) . ' ' . generateRandomWord(5, 6) . ' ' . generateRandomWord(5, 6);
-
-        // Update WLAN configuration
-        $res = $unifi->put(
-            '/proxy/network/api/s/default/rest/wlanconf/' . $wifisData['configuration']['_id'],
-            json_encode(array(
-                'name' => $newSSID,
-                'x_passphrase' => $newPsk,
-            )),
-            'application/json',
-            array()
+        // Fetch list of WLAN networks
+        $wifis = $unifi->get(
+            '/proxy/network/v2/api/site/default/wlan/enriched-configuration'
         );
-    }
+        if (count($configSite) < 1) {
+            throw new Exception('No configuration found');
+        }
 
-} catch (Exception $e) {
-    print 'Exception: ' . $e->getMessage();
+        // Change WLAN keys
+        foreach ($wifis as $wifisI => $wifisData) {
+            if (strpos($wifisData['configuration']['name'], $configSite['wifi-prefix']) !== 0) {
+                continue;
+            }
+
+            // Generate new SSID and PSK
+            $newSSID = $configSite['wifi-prefix'] . ' ' . date('ymd');
+            $newPsk = generateRandomWord(5, 6) . ' ' . generateRandomWord(5, 6) . ' ' . generateRandomWord(5, 6);
+
+            // Update WLAN configuration
+            $res = $unifi->put(
+                '/proxy/network/api/s/default/rest/wlanconf/' . $wifisData['configuration']['_id'],
+                json_encode(array(
+                    'name' => $newSSID,
+                    'x_passphrase' => $newPsk,
+                )),
+                'application/json',
+                array()
+            );
+        }
+
+    } catch (Exception $e) {
+        print 'Exception: ' . $e->getMessage();
+    }
 }
